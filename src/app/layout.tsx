44bf6de2fdb91f1e999c500ac23e93229fcf60b7
matin aliakbari <matin54843590@gmail.com>
2025-09-08 20:38:19 +0330

    Added font vazir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const vazir = localFont({
+  src: '/public/fonts/vazir/Vazir-Medium-FD-WOL.woff2',
+  display: 'swap',
+  style: 'normal',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${vazir.className} antialiased`}
       >
         {children}
       </body>

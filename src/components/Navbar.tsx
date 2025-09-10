afa3bf0f8c5845679e230d2fac544fbe147845f6
matin aliakbari <matin54843590@gmail.com>
2025-09-10 18:28:31 +0330

    Update data in globals.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -125,13 +125,16 @@ export default function Navbar() {
                                 </DropdownMenuTrigger>
                                 <DropdownMenuContent align="end" style={{ direction: "rtl" }}>
                                     <DropdownMenuItem onClick={() => setTheme("light")}>
-                                        <Sun className="mr-2 h-4 w-4" /> روشن
+                                        <Sun className="mr-2 h-4 w-4" />
+                                        <span className="text-[14px]">روشن</span>
                                     </DropdownMenuItem>
                                     <DropdownMenuItem onClick={() => setTheme("dark")}>
-                                        <Moon className="mr-2 h-4 w-4" /> تاریک
+                                        <Moon className="mr-2 h-4 w-4" />
+                                        <span className="text-[14px]">تاریک</span>
                                     </DropdownMenuItem>
                                     <DropdownMenuItem onClick={() => setTheme("system")}>
-                                        <Laptop className="mr-2 h-4 w-4" /> سیستم
+                                        <Laptop className="mr-2 h-4 w-4" />
+                                        <span className="text-[14px]">سیستم</span>
                                     </DropdownMenuItem>
                                 </DropdownMenuContent>
                             </DropdownMenu>
@@ -164,42 +167,65 @@ export default function Navbar() {
                                 </DropdownMenuTrigger>
                                 <DropdownMenuContent align="start" style={{ direction: "rtl" }} className="w-84 p-3">
                                     <div className="flex flex-col gap-y-2">
-                                        <div className="flex items-center justify-between px-2 border-b border-zinc-200 dark:border-zinc-700 pb-2">
-                                            <h4 className="text-md font-normal dark:text-gray-400">اعلان</h4>
-                                            <span className="text-[12px] font-normal bg-indigo-400 dark:bg-indigo-600 p-1 rounded-md text-white">
-                                                {notifications.length} جدید
-                                            </span>
-                                        </div>
-
-                                        <div className="flex flex-col gap-y-2">
-                                            {notifications.map((n) => (
-                                                <div key={n.id} className="flex items-center justify-between gap-x-2 mt-2 border-b last:border-0 border-zinc-200 dark:border-zinc-700 pb-2">
-                                                    {/* بخش پروفایل و متن */}
-                                                    <div className="flex gap-x-2">
-                                                        <div className="w-11 h-11 flex-shrink-0 relative">
-                                                            <Image
-                                                                src={n.avatar}
-                                                                width={44}
-                                                                height={44}
-                                                                alt={`${n.name} avatar`}
-                                                                className="object-cover rounded-full"
-                                                            />
-                                                            <span className="absolute right-1 bottom-1 translate-x-1/4 translate-y-1/4 bg-green-600 w-2 h-2 rounded-full border-2 border-white dark:border-slate-800"></span>
-                                                        </div>
-
-                                                        <div className="flex flex-col space-y-2">
-                                                            <p className="text-[13px]">{n.text}</p>
-                                                            <span className="text-[10px] dark:text-gray-500">{n.time}</span>
-                                                        </div>
-                                                    </div>
-
-                                                    {/* دکمه حذف */}
-                                                    <button onClick={() => handleRemoveNotification(n.id)} className="ml-2 flex items-center justify-center w-6 h-6 rounded-full bg-zinc-100 hover:bg-zinc-200 dark:bg-slate-700 dark:hover:bg-slate-800 transition-colors">
-                                                        <X size={14} strokeWidth={2} className="text-zinc-600 dark:text-zinc-300" />
-                                                    </button>
+                                        {notifications.length > 0 ? (
+                                            <>
+                                                <div className="flex items-center justify-between px-2 border-b border-zinc-200 dark:border-zinc-700 pb-2">
+                                                    <h4 className="text-md font-normal dark:text-gray-400">اعلان</h4>
+                                                    <span className="text-[12px] font-normal bg-indigo-400 dark:bg-indigo-600 p-1 rounded-md text-white">
+                                                        {notifications.length} جدید
+                                                    </span>
                                                 </div>
-                                            ))}
-                                        </div>
+
+                                                <div className="flex flex-col gap-y-2">
+                                                    {notifications.map((n) => (
+                                                        <div key={n.id} className="flex items-center justify-between gap-x-2 mt-2 border-b last:border-0 border-zinc-200 dark:border-zinc-700 pb-2">
+                                                            {/* بخش پروفایل و متن */}
+                                                            <div className="flex gap-x-2">
+                                                                <div className="w-11 h-11 flex-shrink-0 relative">
+                                                                    <Image
+                                                                        src={n.avatar}
+                                                                        width={44}
+                                                                        height={44}
+                                                                        alt={`${n.name} avatar`}
+                                                                        className="object-cover rounded-full"
+                                                                    />
+                                                                    <span className="absolute right-1 bottom-1 translate-x-1/4 translate-y-1/4 bg-green-600 w-2 h-2 rounded-full border-2 border-white dark:border-slate-800"></span>
+                                                                </div>
+
+                                                                <div className="flex flex-col space-y-2">
+                                                                    <p className="text-[13px]">{n.text}</p>
+                                                                    <span className="text-[10px] dark:text-gray-500">{n.time}</span>
+                                                                </div>
+                                                            </div>
+
+                                                            {/* دکمه حذف */}
+                                                            <button onClick={() => handleRemoveNotification(n.id)} className="ml-2 flex items-center justify-center w-6 h-6 rounded-full bg-zinc-100 hover:bg-zinc-200 dark:bg-slate-700 dark:hover:bg-slate-800 transition-colors">
+                                                                <X size={14} strokeWidth={2} className="text-zinc-600 dark:text-zinc-300" />
+                                                            </button>
+                                                        </div>
+                                                    ))}
+                                                    <Link href="/">
+                                                        <span className="text-[14px] text-white dark:bg-blue-600 bg-blue-500 rounded-md mt-2 p-2 block text-center hover:bg-blue-700 transition-colors">خواندن همه اعلان‌ها</span>
+                                                    </Link>
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <div className="px-2 border-b border-zinc-200 dark:border-zinc-700 pb-2">
+                                                    <h4 className="text-md font-normal dark:text-gray-400">اعلان</h4>
+                                                </div>
+
+                                                <div className="flex flex-col items-center justify-center text-blue-600 pt-6 pb-2">
+                                                    <svg className="text-blue-600 dark:text-blue-700 dark:ring-blue-800/50 ring-2 ring-blue-700/50 rounded-full" width="40" height="40" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path opacity="0.5" d="M20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10Z" fill="currentColor"></path>
+                                                        <path d="M10 4.25C10.4142 4.25 10.75 4.58579 10.75 5V11C10.75 11.4142 10.4142 11.75 10 11.75C9.58579 11.75 9.25 11.4142 9.25 11V5C9.25 4.58579 9.58579 4.25 10 4.25Z" fill="currentColor"></path>
+                                                        <path d="M10 15C10.5523 15 11 14.5523 11 14C11 13.4477 10.5523 13 10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15Z" fill="currentColor"></path>
+                                                    </svg>
+                                                    <p className="mt-4 dark:text-gray-400 text-black">اطلاعاتی موجود نیست</p>
+                                                </div>
+                                            </>
+                                        )}
+
                                     </div>
                                 </DropdownMenuContent>
                             </DropdownMenu>
